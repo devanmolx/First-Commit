cd5b1b44cb911690ac7032d3a7cfbeba687568f6
Anmol Garg <anmolgarg271005@gmail.com>
2025-10-23 11:39:21 +0530

    design fix

--- issue-pulse/components/IssueModel.tsx
+++ issue-pulse/components/IssueModel.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogClose } from "@/components/ui/dialog";
 import { IssueType } from "@/types/types";
 import LatestIssuesTable from "./LatestIssuesTable";
+import { X } from "lucide-react";
 
 interface IssuesModalProps {
     open: boolean;
@@ -15,8 +16,13 @@ const IssuesModal: React.FC<IssuesModalProps> = ({ open, onClose, projectName, i
     return (
         <Dialog open={open} onOpenChange={onClose}>
             <DialogContent className="!max-w-[95vw] !w-full !h-[90vh] bg-slate-900 border-slate-800 overflow-y-auto">
-                <DialogHeader>
+                <DialogHeader className="flex flex-row items-center justify-between ">
                     <DialogTitle className="text-white">Issues for {projectName}</DialogTitle>
+                    <DialogClose asChild>
+                        <button className="text-white cursor-pointer">
+                            <X className="w-5 h-5" />
+                        </button>
+                    </DialogClose>
                 </DialogHeader>
                 <LatestIssuesTable issues={issues} />
             </DialogContent>
